27fec1c24b7ac6538ea562e37010cf51cc0c7505
Tru-Apicella <truapicella@gmail.com>
2022-10-04 11:08:22 -0400

    fixed stuff/added remove_junk method

--- EmailClass.py
+++ EmailClass.py
@@ -1,7 +1,6 @@
 import string
 import itertools as it
 
-from sympy import true
 #format for spam and not spam arrays will be as following spam[testing/training[each email[words in email]]]
 testing = []
 training = []
@@ -15,7 +14,7 @@ def make_arrays(array, type):
     j = 0
     while i <= 25:
         try:
-            s1,s2,s3,s4 = 'C:/Users/zoetr/Desktop/email/email/',type, str(i) , '.txt'
+            s1,s2,s3,s4 = 'C:/Users/tru/Desktop/email/email/',type, str(i) , '.txt'
             s = ''.join([s1,s2,s3,s4])
             f = open(s, 'r')
         except:
@@ -38,34 +37,44 @@ def make_arrays(array, type):
             tmp = list(filter(None,tmp)) 
             array[1].append(tmp)
 
+def remove_junk(x):
+    bool = True
+    if x in ['a','an','and','are','as','at','be','by','for','from','has','he','in','it','its','of','on','that','the','to','was','were', 'will', 'with']:
+        bool = False
+    return bool
+
+def create_vocab():
+    vocabulary = []
+    for x in all_spam:
+        bool = remove_junk(x)
+        if bool ==True:
+            check = 0
+            for y in all_not_spam:
+                if x == y:
+                    check = 1
+                    break
+            if check == 0:
+                vocabulary.append(x)
+
+    for x in all_not_spam:
+        bool = remove_junk(x)
+        if bool ==True:
+            check = 0
+            for y in all_spam:
+                if x == y:
+                    check = 1
+                    break
+            if check == 0:
+                vocabulary.append(x)
+    return vocabulary
+
 make_arrays(not_spam, "normal/")
 make_arrays(spam, "spam/")
 tmp = list(it.chain.from_iterable(spam))
 all_spam = list(it.chain.from_iterable(tmp))
 tmp = list(it.chain.from_iterable(not_spam))
 all_not_spam = list(it.chain.from_iterable(tmp))
-vocabulary = []
 
-for x in all_spam:
-    check = 0
-    for y in all_not_spam:
-        if x == y:
-            check = 1
-            break
-    if check == 0:
-        vocabulary.append(x)
+vocabulary = create_vocab()
 
-for x in all_not_spam:
-    check = 0
-    for y in all_spam:
-        if x == y:
-            check = 1
-            break
-    if check == 0:
-        vocabulary.append(x)
-
-i = 0
-while i < len(vocabulary):
-    if i % 25 == 0:
-        print (vocabulary[i])
-    i+=1
+print("breakpoint")
