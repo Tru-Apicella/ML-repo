a709f359d905a9dc0c4224d6f3dc53d93939cd1e
Tru-Apicella <truapicella@gmail.com>
2022-10-04 00:29:22 -0400

    vocab list started

--- EmailClass.py
+++ EmailClass.py
@@ -1,9 +1,18 @@
 import string
-not_spam = []
-spam = []
+import itertools as it
+
+from sympy import true
+#format for spam and not spam arrays will be as following spam[testing/training[each email[words in email]]]
+testing = []
+training = []
+stesting = []
+straining = []
+not_spam = [training,testing]
+spam = [straining,stesting]
 
 def make_arrays(array, type):
     i = 1
+    j = 0
     while i <= 25:
         try:
             s1,s2,s3,s4 = 'C:/Users/zoetr/Desktop/email/email/',type, str(i) , '.txt'
@@ -13,16 +22,50 @@ def make_arrays(array, type):
             i+=1
             continue
         i+=1
-        for line in f:
-            for word in line.split():
-                array.append(word)
-    i = 0
-    for count in enumerate(array):
-        x = count[0]
-        array[x] = array[x].translate(str.maketrans('','',string.punctuation))
-    
+        tmp = []
+        if (j < 5):
+            for line in f:
+                for word in line.split():
+                    tmp.append(word.translate(str.maketrans('','',string.punctuation)))
+            tmp = list(filter(None,tmp))
+            array[0].append(tmp)
+
+            j+=1
+        else:
+            for line in f:
+                for word in line.split():
+                    tmp.append(word.translate(str.maketrans('','',string.punctuation)))
+            tmp = list(filter(None,tmp)) 
+            array[1].append(tmp)
 
 make_arrays(not_spam, "normal/")
 make_arrays(spam, "spam/")
-spam = list(filter(None,spam))
-not_spam = list(filter(None,not_spam))
+tmp = list(it.chain.from_iterable(spam))
+all_spam = list(it.chain.from_iterable(tmp))
+tmp = list(it.chain.from_iterable(not_spam))
+all_not_spam = list(it.chain.from_iterable(tmp))
+vocabulary = []
+
+for x in all_spam:
+    check = 0
+    for y in all_not_spam:
+        if x == y:
+            check = 1
+            break
+    if check == 0:
+        vocabulary.append(x)
+
+for x in all_not_spam:
+    check = 0
+    for y in all_spam:
+        if x == y:
+            check = 1
+            break
+    if check == 0:
+        vocabulary.append(x)
+
+i = 0
+while i < len(vocabulary):
+    if i % 25 == 0:
+        print (vocabulary[i])
+    i+=1
